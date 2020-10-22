47723f44d31ea5430966a1dd31041e16d4aac83a
Hugman <contact.hugman@gmail.com>
2020-10-22 23:09:40 +0200

    Initial commit

---
+++ src/main/java/com/hugman/cataclysms_of_herobrine/CataclysmsOfHerobrine.java
@@ -0,0 +1,16 @@
+package com.hugman.cataclysms_of_herobrine;
+
+import com.hugman.dawn.api.creator.ModData;
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class CataclysmsOfHerobrine implements ModInitializer {
+	public static final ModData MOD_DATA = new ModData("cataclysms_of_herobrine");
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
